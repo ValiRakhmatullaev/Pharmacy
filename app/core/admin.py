6f3782df2d87a7e8f9a36b7df825188309d16948
Vali <valijon1998valijon@icloud.com>
2020-07-15 23:50:18 +0300

    add file pharmacymodels.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+
+
+
+class CustomUer(AbstractUser):
+    group=models.ManyToManyField('core.Group')
+
+class Group(models.Model):
+
 
 # Register your models here.
